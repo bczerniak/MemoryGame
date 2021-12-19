0c8ee5433ea4e5ce4d8b944d6748f06a29357cb2
flapjack <bart.czerniak@gmail.com>
2021-12-19 18:03:09 +0100

    Almost there

--- app/src/main/java/com/mygame/dopasujdwa/MainActivity.java
+++ app/src/main/java/com/mygame/dopasujdwa/MainActivity.java
@@ -1,46 +1,48 @@
 package com.mygame.dopasujdwa;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.Bindable;
-import androidx.databinding.DataBindingUtil;
-
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private ActivityMainBinding binding;
     HashMap<Integer, Integer> buttonImagePairs = new HashMap<>();
     ArrayList<Integer> images = new ArrayList<>();
     ArrayList<ImageButton> buttons = new ArrayList<>();
 
+    long startTimer;
+
     ImageButton firstSelectedButton;
     ImageButton secondSelectedButton;
-
-//    private boolean canClickMemoryButtons = false;
-//
-//    @Bindable
-//    public Boolean getMemoryButtonsClickable() {
-//        return  canClickMemoryButtons;
-//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        initializeImages();
+        initializeGame();
     }
 
-    public void initializeImages() {
+    public void initializeGame() {
+        startTimer = System.currentTimeMillis();
+
         buttonImagePairs.clear();
         buttons.clear();
         images.clear();
 
+        initializeImages();
+        initializeButtons();
+        initializePairs();
+    }
+
+    public void initializeImages() {
         images.add(R.drawable.cat);
         images.add(R.drawable.cat);
         images.add(R.drawable.dog);
@@ -54,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
         images.add(R.drawable.wolf);
         images.add(R.drawable.wolf);
 
+        Collections.shuffle(images);
+    }
+
+    public void initializeButtons() {
         buttons.add(findViewById(R.id.button0));
         buttons.add(findViewById(R.id.button1));
         buttons.add(findViewById(R.id.button3));
@@ -66,9 +72,9 @@ public class MainActivity extends AppCompatActivity {
         buttons.add(findViewById(R.id.button9));
         buttons.add(findViewById(R.id.button10));
         buttons.add(findViewById(R.id.button11));
+    }
 
-//        Collections.shuffle(images);
-
+    public void initializePairs() {
         for (int i = 0; i < buttons.size(); i++) {
             ImageButton button = buttons.get(i);
             buttonImagePairs.put(button.getId(), images.get(i));
@@ -76,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onMemoryButtonClick (View view) {
-
 
         ImageButton selectedButton = (ImageButton) view;    //castowanie/unboxing
         int buttonId = selectedButton.getId();
@@ -99,23 +104,59 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (firstSelectedButton.getBackground().getConstantState() != secondSelectedButton.getBackground().getConstantState()) {    //poczytać o getConstantState()
+            setClickable(false);
             Handler handler = new Handler();
             handler.postDelayed(() -> {
                 firstSelectedButton.setBackgroundResource(R.drawable.znakzapytania);
                 secondSelectedButton.setBackgroundResource(R.drawable.znakzapytania);
                 firstSelectedButton = null;
                 secondSelectedButton = null;
-            }, 500);
+                setClickable(true);
+            }, 700);
         } else {
             buttonImagePairs.remove(firstSelectedButton.getId());
             buttonImagePairs.remove(secondSelectedButton.getId());
             firstSelectedButton = null;
             secondSelectedButton = null;
         }
-        //sprawdzic czy buttonImagePairs są puste, jeśli tak to zwycięstwo
+
+        if (buttonImagePairs.isEmpty()) {
+            setClickable(false);
+
+            Toast toast = Toast.makeText(this, "Gratulacje, zwyciężyłeś!!!", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0,0);
+            toast.show();
+
+            TextView showTime = findViewById(R.id.showTime);
+            showTime.setText("Twój czas wyniósł:  " + (System.currentTimeMillis() - startTimer)/1000 + " sekund");
+
+            Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                findViewById(R.id.playAgainButton).setVisibility(View.VISIBLE);
+                findViewById(R.id.showTime).setVisibility(View.VISIBLE);
+            }, 2000);
+        }
     }
 
     public void playAgain (View view) {
-        initializeImages();
+        setInitialButtonsBackground();
+        initializeGame();
+        setClickable(true);
+        findViewById(R.id.playAgainButton).setVisibility(View.INVISIBLE);
+        findViewById(R.id.showTime).setVisibility(View.INVISIBLE);
+    }
+
+    public void setClickable (boolean setClickable) {
+        for (int i = 0; i < buttons.size(); i++){
+            buttons.get(i).setClickable(setClickable);
+        }
+    }
+
+    private void setInitialButtonsBackground() {
+        for (int i = 0; i < buttons.size(); i++){
+            buttons.get(i).setBackgroundResource(R.drawable.znakzapytania);
+        }
     }
 }
+
+//może dodać porównanie z najlepszym czasem
--- app/src/main/java/com/mygame/dopasujdwa/StartingPageActivity.java
+++ app/src/main/java/com/mygame/dopasujdwa/StartingPageActivity.java
@@ -13,9 +13,15 @@ public class StartingPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.starting_page);
+        animation();
     }
 
-    public void goToGame(View v){
+    public void goToGame(View view){
         startActivity(new Intent(StartingPageActivity.this, MainActivity.class));
+    }
+
+    public void animation() {
+        TextView title = findViewById(R.id.textView);
+        title.animate().translationZ(0).rotationX(-1080).setDuration(9000);
     }
 }
